be21ec2c0434f2f5d298652699e2108e2d4befaf
Raunak Khanna <raunaksachinkhanna@gmail.com>
2025-02-15 21:55:01 -0800

    I tried doing the last part ugh!

---
+++ Assets/FallTrigger.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FallTrigger : MonoBehaviour
+{
+    public UnityEvent OnPinFall = new();
+    public bool isPinFallen = false;
+
+    private void OnTriggerEnter(Collider triggeredObject)
+    {
+        if (triggeredObject.CompareTag("GroundTag") && !isPinFallen)
+        {
+            isPinFallen = true;
+            OnPinFall?.Invoke();
+            Debug.Log($"{gameObject.name} is fallen");
+        }
+    }
+}
